1ece4ed651152edfa1871ceeb0a20f263f07fff5
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-29 12:55:08 -0800

    Comment

--- LeetcodeSolversCSharp/LeetcodeSolversCSharp/IsPalindromeExtensions.cs
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/IsPalindromeExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class IsPalindromeExtensions
     {
+        // https://leetcode.com/problems/palindrome-number
+        /// <summary>
+        /// Checks whether the integer is a palindromic string when converted.
+        /// Note: always false for negative numbers
+        /// </summary>
         public static bool IsPalindrome(this int x)
         {
             string forwardOrder = x.ToString();
