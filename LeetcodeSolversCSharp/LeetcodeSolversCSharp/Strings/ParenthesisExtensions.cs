a286d06aed317e4f69b5ca4784d8a57a6aac11bb
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-29 14:29:24 -0800

    Solved Valid Parenthesis

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/ParenthesisExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Strings
+{
+    public static class ParenthesisExtensions
+    {
+        public static bool IsValid(this string s)
+        {
+            var stack = new Stack<char>();
+            char previous;
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                    case '[':
+                    case '{':
+                        stack.Push(s[i]);
+                        break;
+                    case ')':
+                    case ']':
+                    case '}':
+                        if (stack.Count == 0)
+                            return false;
+
+                        previous = stack.Pop();
+                        if (!DoParenthesisMatch(previous, s[i]))
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(s), "input can only contain parenthesis characters");
+                }
+            }
+            return stack.Count == 0;
+        }
+
+        private static bool DoParenthesisMatch(char opener, char closer)
+        {
+            switch (opener)
+            {
+                case '(':
+                    return closer == ')';
+                case '[':
+                    return closer == ']';
+                case '{':
+                    return closer == '}';
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(opener));
+            }
+        }
+    }
+
+    
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/ParenthesisExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using LeetcodeSolversCSharp;
+using LeetcodeSolversCSharp.Strings;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    internal class ParenthesisExtensionsTests
+    {
+        [TestCase("()", true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("(]", false)]
+        [TestCase("([)]", false)]
+        [TestCase("{[]}", true)]
+        public void ValidateExampleBehavior(string input, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, input.IsValid());
+        }
+    }
+}
