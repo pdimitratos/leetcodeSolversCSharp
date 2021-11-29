35733cb5483aff0bf3f304c219591376c7d5d348
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-29 14:09:39 -0800

    Solved Longest Common Prefix

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/LongestCommonPrefixExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.strings
+{
+    public static class LongestCommonPrefixExtensions
+    {
+        //https://leetcode.com/problems/longest-common-prefix/
+        public static string LongestCommonPrefix(this string[] strs)
+        {
+            string longestCommonPrefix = string.Empty;
+            for(int i = 0; i < strs.Select(str => str.Length).Min(); i++)
+            {
+                char current = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != current)
+                    {
+                        return longestCommonPrefix;
+                    }
+                }
+                longestCommonPrefix += current;
+            }
+            return longestCommonPrefix;
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/LongestCommonPrefixTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using LeetcodeSolversCSharp;
+using LeetcodeSolversCSharp.strings;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    public class LongestCommonPrefixTests
+    {
+        [TestCase(new string[] { "flower", "flow", "flight" }, "fl")]
+        [TestCase(new string[] { "dog", "racecar", "car" }, "")]
+        public void ValidateExampleBehavior(string[] input, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, input.LongestCommonPrefix());
+        }
+    }
+}
