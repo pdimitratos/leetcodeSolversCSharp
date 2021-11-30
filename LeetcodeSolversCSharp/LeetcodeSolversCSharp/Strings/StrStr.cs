e52786c71ff5dcd90caa95fad8f1ca70ab851605
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-29 16:13:53 -0800

    Completed strstr

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/StrStr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Strings
+{
+    public class StrStrSolver
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == string.Empty) return 0;
+
+            // Not what I'd do in an interview, but...
+            return haystack.IndexOf(needle);
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/StrStrTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetcodeSolversCSharp.Strings;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    internal class StrStrTests
+    {
+        [TestCase("hello", "ll", 2)]
+        [TestCase("aaaaa", "bba", -1)]
+        [TestCase("", "", 0)]
+        public void ValidateExampleBehavior(string inputHaystack, string inputNeedle, int expectedResult)
+        {
+            var objectUnderTest = new StrStrSolver();
+
+            Assert.AreEqual(expectedResult, objectUnderTest.StrStr(inputHaystack, inputNeedle));
+        }
+    }
+}
