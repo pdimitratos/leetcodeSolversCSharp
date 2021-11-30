f5ae790d045759c9e7b932c2c67e998e161580c4
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-30 11:58:25 -0800

    Longest palindromic substring

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/PalindromeExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Strings
+{
+    public static class PalindromeExtensions
+    {
+        // https://leetcode.com/problems/longest-palindromic-substring
+        public static string LongestPalindrome(this string s)
+        {
+            string longestPalindrome = string.Empty;
+            string reversedString = new string(s.Reverse().ToArray());
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < reversedString.Length - i; j++)
+                {
+                    bool hasPalindrome = false;
+                    for (
+                        int palindromeLookaheadIndex = 0;
+                        palindromeLookaheadIndex < reversedString.Length - j - i;
+                        palindromeLookaheadIndex++)
+                    {
+                        if (reversedString[j + palindromeLookaheadIndex] != s[i + palindromeLookaheadIndex])
+                        {
+                            hasPalindrome = false;
+                            break;
+                        }
+                        hasPalindrome = true;
+                    }
+
+                    if (hasPalindrome)
+                    {
+                        string currentPalindrome = reversedString.Substring(j, reversedString.Length - i - j);
+                        longestPalindrome = currentPalindrome.Length > longestPalindrome.Length
+                            ? currentPalindrome
+                            : longestPalindrome;
+                    }
+                }
+            }
+            return longestPalindrome;
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/PalindromeExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetcodeSolversCSharp.Strings;
+using NUnit.Framework;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    public class PalindromeExtensionsTests
+    {
+        [TestCase("babad", "bab")]
+        [TestCase("cbbd", "bb")]
+        [TestCase("a", "a")]
+        [TestCase("ac", "a")]
+        [TestCase("aacabdkacaa", "aca")]
+        public void ValidateExampleBehavior_AddBinary(string input, string expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, input.LongestPalindrome());
+        }
+    }
+}
