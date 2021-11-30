32d106c00abd979daaa59e6fa6d4b0b45ab6ecf1
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-29 16:01:15 -0800

    Solved remove duplicates from sorted array

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/DataStructures/ArrayExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.DataStructures
+{
+    public static class ArrayExtensions
+    {
+        public static int RemoveDuplicates(this int[] nums)
+        {
+            int currentIndex = 0;
+            int displacement = 0;
+            int previousValue = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == previousValue)
+                {
+                    displacement++;
+                }
+                else
+                {
+                    nums[i - displacement] = nums[i];
+                    previousValue = nums[i];
+                    currentIndex++;
+                }
+            }
+
+
+            return currentIndex;
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/DataStructures/ArrayExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using LeetcodeSolversCSharp;
+using LeetcodeSolversCSharp.DataStructures;
+
+namespace LeetcodeSolversCSharpTests.DataStructures
+{
+    internal class ArrayExtensionsTests
+    {
+        [TestCase(new int[] { 1, 1, 2 }, 2)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void ValidateExampleBehavior(int[] input, int expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, input.RemoveDuplicates());
+        }
+    }
+}
