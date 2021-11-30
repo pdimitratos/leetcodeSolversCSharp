876f74e8969c902a256f91bddb3c470256e5cbbe
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-30 13:51:20 -0800

    Solved reverse integer

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Integers/IntegerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Integers
+{
+    public static class IntegerExtensions
+    {
+        public static int Reverse(this int x)
+        {
+            bool isNegative = x < 0;
+            x = isNegative ? -x : x;
+            int workingTotal = 0;
+            int currentPlaceValue = 1;
+            Stack<int> digitStack = new Stack<int>();
+            while (x > 0)
+            {
+                digitStack.Push(x % 10);
+                x = x / 10;
+            }
+
+            while (digitStack.TryPop(out int newDigit))
+            {
+                int newDigitValue = (newDigit * currentPlaceValue);
+                if (newDigit != 0
+                    && (currentPlaceValue > int.MaxValue / newDigit
+                        || int.MaxValue - newDigitValue < workingTotal))
+                {
+                    return 0;
+                }
+                workingTotal += newDigitValue;
+                currentPlaceValue = currentPlaceValue * 10;
+            }
+
+            return isNegative
+                ? -1 * workingTotal
+                : workingTotal;
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Integers/IntegerExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LeetcodeSolversCSharp;
+using LeetcodeSolversCSharp.DataStructures;
+using LeetcodeSolversCSharp.Integers;
+
+namespace LeetcodeSolversCSharpTests.Integers
+{
+    internal class IntegerExtensionsTests
+    {
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(0, 0)]
+        [TestCase(1534236469, 0)]
+        public void ValidateExampleBehavior_RemoveDuplicates(int input, int expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, input.Reverse());
+        }
+    }
+}
