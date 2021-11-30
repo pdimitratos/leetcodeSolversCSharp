059db7ee20333a6d7ff4960109ce0f62f306f77a
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-30 11:29:12 -0800

    Solved Add Binary

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/BinaryStrings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Strings
+{
+    public static class BinaryStrings
+    {
+        public static string AddBinary(string a, string b)
+        {
+            var previousDigits = new Stack<char>();
+            bool carryForward = false;
+            int shortestStringLength = Math.Min(a.Length, b.Length);
+            for (int i = 1; i <= shortestStringLength; i++)
+            {
+                char valA = a[a.Length - i];
+                char valB = b[b.Length - i];
+                char digitToPush = valA == valB
+                    ? '0'
+                    : '1';
+                if (carryForward)
+                {
+                    carryForward = digitToPush == '1'
+                        || valA == '1' && valB == '1';
+                    digitToPush = digitToPush == '0' ? '1' : '0';
+                }
+                else
+                {
+                    carryForward = valA == '1' && valB == '1';
+                }
+                previousDigits.Push(digitToPush);
+            }
+            string longestString = a.Length > b.Length ? a : b;
+            for (int i = longestString.Length - 1 - shortestStringLength; i >= 0; i--)
+            {
+                char digitToPush = longestString[i];
+                if (carryForward)
+                {
+                    carryForward = digitToPush == '1';
+                    digitToPush = digitToPush == '0' ? '1' : '0';
+                }
+                previousDigits.Push(digitToPush);
+            }
+            if (carryForward)
+            {
+                previousDigits.Push('1');
+            }
+            return new string(previousDigits.ToArray());
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/BinaryStringsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetcodeSolversCSharp.Strings;
+using NUnit.Framework;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    public class BinaryStringsTests
+    {
+
+        [TestCase("11", "1", "100")]
+        [TestCase("1010", "1011", "10101")]
+        [TestCase("1111", "1111", "11110")]
+        public void ValidateExampleBehavior_AddBinary(string inputA, string inputB, string expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, BinaryStrings.AddBinary(inputA, inputB));
+        }
+    }
+}
