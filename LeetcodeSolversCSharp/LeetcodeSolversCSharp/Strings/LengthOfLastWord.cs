82336ba28e877e0b56ad75f4116b98be802684c0
Philip Dimitratos <p.dimitratos@gmail.com>
2021-11-30 10:50:11 -0800

    Solved length of last word

---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharp/Strings/LengthOfLastWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolversCSharp.Strings
+{
+    public class LengthOfLastWordSolver
+    {
+        public int LengthOfLastWord(string s)
+        {
+            return s.Trim().Split(' ').Last().Length;
+        }
+    }
+}
---
+++ LeetcodeSolversCSharp/LeetcodeSolversCSharpTests/Strings/LengthOfLastWordTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using LeetcodeSolversCSharp;
+using LeetcodeSolversCSharp.strings;
+using LeetcodeSolversCSharp.Strings;
+
+namespace LeetcodeSolversCSharpTests.Strings
+{
+    public class LengthOfLastWordTests
+    {
+        [TestCase("Hello World", 5)]
+        [TestCase("   fly me   to   the moon  ", 4)]
+        [TestCase("luffy is still joyboy", 6)]
+        public void ValidateExampleBehavior(string input, int expectedResult)
+        {
+            var objectUnderTest = new LengthOfLastWordSolver();
+
+            Assert.AreEqual(expectedResult, objectUnderTest.LengthOfLastWord(input));
+        }
+    }
+}
