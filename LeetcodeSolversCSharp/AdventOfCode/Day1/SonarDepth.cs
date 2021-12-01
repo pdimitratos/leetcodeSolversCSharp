524bc214c125e7abac59d250d801030744999ef4
Philip Dimitratos <p.dimitratos@gmail.com>
2021-12-01 11:03:44 -0800

    Finished part 2

--- LeetcodeSolversCSharp/AdventOfCode/Day1/SonarDepth.cs
+++ LeetcodeSolversCSharp/AdventOfCode/Day1/SonarDepth.cs
@@ -25,5 +25,30 @@ namespace AdventOfCode.Day1
 
             return depthIncreases;
         }
+
+        // https://adventofcode.com/2021/day/1#part2
+        public int CountWindowedDepthIncreases(int[] orderedDepthMeasurements)
+        {
+            if (orderedDepthMeasurements.Length <= 2)
+                throw new ArgumentOutOfRangeException(nameof(orderedDepthMeasurements));
+
+            int[] orderedWindows = new int[orderedDepthMeasurements.Length - 2];
+            orderedWindows[0] = orderedDepthMeasurements.Take(3).Sum();
+            int depthIncreases = 0;
+            int previousMeasurement = int.MaxValue;
+
+            for (int i = 1; i < orderedDepthMeasurements.Length - 2; i++)
+            {
+                orderedWindows[i] = orderedWindows[i - 1]
+                    - orderedDepthMeasurements[i - 1]
+                    + orderedDepthMeasurements[i + 2];
+                if (orderedWindows[i - 1] < orderedWindows[i])
+                {
+                    depthIncreases++;
+                }
+            }
+
+            return depthIncreases;
+        }
     }
 }
