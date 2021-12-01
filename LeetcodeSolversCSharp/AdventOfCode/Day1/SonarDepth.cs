327d21c59c8a83e0ef7d04d2e45f04adaa8ac93d
Philip Dimitratos <p.dimitratos@gmail.com>
2021-12-01 10:48:37 -0800

    Solved first advent of code problem

---
+++ LeetcodeSolversCSharp/AdventOfCode/Day1/SonarDepth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day1
+{
+    public class SonarDepth
+    {
+        // https://adventofcode.com/2021/day/1
+        public int CountDepthIncreases(int[] orderedDepthMeasurements)
+        {
+            int depthIncreases = 0;
+            int previousMeasurement = int.MaxValue;
+
+            for (int i = 0; i < orderedDepthMeasurements.Length; i++)
+            {
+                if (previousMeasurement < orderedDepthMeasurements[i])
+                {
+                    depthIncreases++;
+                }
+                previousMeasurement = orderedDepthMeasurements[i];
+            }
+
+            return depthIncreases;
+        }
+    }
+}
